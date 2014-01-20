8f98cdd6b6c497b213fff5ac018f504df7d20c58
Andy Truong <andy@go1.com.au>
2014-01-20 12:00:35 +0700

    Compatiple with 2.6

---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2013110500;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2013110500;        // Requires this Moodle version
+$plugin->component = 'auth_drupalservices';       // Full name of the plugin (used for diagnostics)
